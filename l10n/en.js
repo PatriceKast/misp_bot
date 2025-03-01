52a9157b84de6d91f34492b0d62c65a4004f5770
Patrice Kast <patrice.kast@kastgroup.com>
2025-03-01 15:55:26 +0100

    added translation for misp bot

--- l10n/en.js
+++ l10n/en.js
@@ -4,9 +4,9 @@ OC.L10N.register(
         "%s (guest)" : "%s (guest)",
         "__language_name__"  : "English (British English)",
         "bot_description": "Use @misp and a list of IPv4 addresses to import and submit new IoCs to a conncted MISP IoC Sharing Plattform.",
-        "error_private_ips (%s)"  : "Failure: There were private IPv4 addresses submitted for submission:\n- (%s)",
+        "error_private_ips (%s)"  : "Failure: There were private IPv4 addresses submitted for submission:\n- %s",
         "error_no_ips_found" : "Failure: There were no valid IPv4 addresses submitted for submission",
-        "success_ip_submission (%s)" : "Success: The following IPv4 addresses were submitted to the connect MISP IoC Sharing Plattform:\n- (%s)"
+        "success_ip_submission (%s)" : "Success: The following IPv4 addresses were submitted to the connect MISP IoC Sharing Plattform:\n- %s"
     },
     "nplurals=2; plural=(n != 1);"
 );
